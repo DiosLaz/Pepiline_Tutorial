89b3e60c453ce4d27c3d8b1b54aa7e823083044a
DiosLaz <67984004+DiosLaz@users.noreply.github.com>
2025-01-18 10:14:02 +0100

    added login controller

---
+++ Pipeline Tutorial/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pipeline_Tutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        public AccountController()
+        {
+            
+        }
+        public async Task<IActionResult> Login()
+        {
+            return Ok("Works fine");
+        }
+    }
+}
