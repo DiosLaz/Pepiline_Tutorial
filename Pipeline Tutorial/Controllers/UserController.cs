570148154aa42a8ab943e161114a134b8bdce82a
DiosLaz <67984004+DiosLaz@users.noreply.github.com>
2025-01-18 21:40:56 +0100

    users added

---
+++ Pipeline Tutorial/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pipeline_Tutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("/User")]
+        public IActionResult Users(string username)
+        {
+            return( Ok(username));
+        }
+    }
+}
